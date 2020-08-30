95673f99c192169966dabacee1a85d8aa9bca225
Cengizhan KÖSE <kose651@gmail.com>
2020-08-30 13:09:35 +0300

    Search Input Component styles created

---
+++ src/components/Input/SearchInput/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  input: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+    fontFamily: 'Kanit',
+  },
+  sameIcon: {padding: 10, paddingRight: 40},
+  searchSection: {
+    flexDirection: 'row-reverse',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%',
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+  },
+});
