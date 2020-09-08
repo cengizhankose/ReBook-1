cac31d124c55270b64d55e39f27e445dc9f9004e
Cengizhan KÖSE <kose651@gmail.com>
2020-09-08 21:23:34 +0300

    homescreen template to test card items

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,71 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Image, Text, ImageBackground} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import LinearGradient from 'react-native-linear-gradient';
+
+import Logo from '../../svg/LogoSvg';
+import CardItem from '../../components/CardItem/index';
+import CardItemMini from '../../components/CardItemMini/index';
+import {styles} from './styles';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const index = () => {
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Image source={require('../../img/dummy.png')} />
+        </TouchableOpacity>
+        <Logo />
+      </View>
+      <View style={styles.body}>
+        <Text
+          style={{
+            color: '#FFAC81',
+            fontSize: 18,
+            fontWeight: 'bold',
+          }}>
+          Po<Text style={{color: 'black'}}>püler </Text>
+          <Text
+            style={{
+              color: '#FF928B',
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}>
+            Ki<Text style={{color: 'black'}}>taplar</Text>
+          </Text>
+        </Text>
+        <CardItemMini />
+      </View>
+      <View style={styles.footer}>
+        <Text
+          style={{
+            color: '#FFAC81',
+            fontSize: 18,
+            fontWeight: 'bold',
+          }}>
+          Ye<Text style={{color: 'black'}}>ni </Text>
+          <Text
+            style={{
+              color: '#FFAC81',
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}>
+            Ek<Text style={{color: 'black'}}>lenen </Text>
+          </Text>
+          <Text
+            style={{
+              color: '#FF928B',
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}>
+            Ki<Text style={{color: 'black'}}>taplar</Text>
+          </Text>
+        </Text>
+
+        <CardItem/>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,3 +1,7 @@
 import {StyleSheet} from 'react-native';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  header: {flex: 1,flexDirection:"row",alignItems: 'center',justifyContent:"space-between",margin:"5%",marginBottom:"3%"},
+  body: {flex: 3,marginLeft:"5%"},
+  footer: {flex: 5,marginLeft:"5%"},
+});
