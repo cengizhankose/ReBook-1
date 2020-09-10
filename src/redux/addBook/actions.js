aa825d89950e1d73eb8921d756cdf1d2ef5b1d0c
Taha Zeybek <tahazeybek5@gmail.com>
2020-09-11 01:21:40 +0300

    some changes

--- src/redux/addBook/actions.js
+++ src/redux/addBook/actions.js
@@ -1,5 +1,6 @@
 import {ADD_BOOK, ADD_BOOK_FAILD, ADD_BOOK_SUCCESS} from './types';
 import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
 import {Alert} from 'react-native';
 
 export const addBook = (params) => {
@@ -12,6 +13,28 @@ export const addBook = (params) => {
       .add(params)
       .catch((err) => {
         console.log(err);
+      })
+      .then((res) => {
+        const reference = storage().ref(`/products/${res.id}`);
+        console.log('referancer', reference);
+        console.log('image uri....', params.image.uri);
+
+        reference.putFile(params.image.uri).then(() => {
+          console.log('birinci giriş');
+          reference.getDownloadURL().then((imageURL) => {
+            console.log('asdurllll', imageURL);
+
+            firestore()
+              .collection('Products')
+              .doc(res.id)
+              .update(params)
+              .then((res) => {
+                console.log(res);
+                //dispatch({ type: ADD_TWEET_SUCCESS, payload: params })
+                //RootNavigation.pop()
+              });
+          });
+        });
       });
   };
 };
--- src/screens/AddBook/index.js
+++ src/screens/AddBook/index.js
@@ -1,5 +1,13 @@
 import React, {useState, Component} from 'react';
-import {View, Text, TextInput, StyleSheet, Dimensions} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 import Input from '../../components/Input/index';
 import Button from '../../components/Button/index';
 import {addBook} from '../../redux/addBook/actions';
@@ -13,18 +21,54 @@ class index extends Component {
     content: 'deneme1',
     price: '40',
     author: 'George Orwell',
+    avatarSource: null,
   };
 
   addBookHandler = async (params) => {
     await this.props.addBook(params);
     this.props.navigation.navigate('Search');
   };
+
+  chooseBook = () => {
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response.uri);
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = {uri: response.uri};
+
+        this.setState({
+          avatarSource: source,
+        });
+      }
+    });
+  };
+
+  uploadImage = () => {};
+
   render() {
     const {title, content, price, author} = this.state;
-    console.log('gelen user', this.props.uid);
+
+    console.log('gelen user', this.props.avatarSource);
     return (
       <View style={styles.addBookView}>
         <Text style={styles.addBookTitle}>Kitap Ekle</Text>
+        <Image
+          style={{width: 30, height: 30}}
+          source={this.state.avatarSource}
+        />
         <Input
           style={{marginBottom: 20, borderColor: '#818081', borderWidth: 1}}
           placeholder={'Kitap İsmi'}
@@ -51,6 +95,7 @@ class index extends Component {
           value={author}
           onChangeText={(value) => this.setState({author: value})}
         />
+
         <Button
           text="Kitap Ekle"
           onPress={() => {
@@ -59,9 +104,16 @@ class index extends Component {
               content,
               price,
               author,
+              image: this.state.avatarSource,
               uid: this.props.uid,
             };
             this.addBookHandler(params);
+          }}
+        />
+        <Button
+          text="resim Ekle"
+          onPress={() => {
+            this.chooseBook();
           }}
         />
       </View>
